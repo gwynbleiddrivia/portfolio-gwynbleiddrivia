edae3ffb341ab18e6ea100bc135b336727a42023
gwynbleiddrivia <yasinzme2@gmail.com>
2023-06-28 23:36:21 +0600

    project animation added, skills name added

--- src/homePages/DisplayProjects.jsx
+++ src/homePages/DisplayProjects.jsx
@@ -1,12 +1,19 @@
 
 const DisplayProjects = () => {
 	return (
-		<div className="flex flex-col justify-between effect-logo rounded-lg p-2 gap-1">
-			<div className="relative w-52 h-28 overflow-hidden rounded-lg">
-			  <div className="absolute top-0 left-0 w-full h-full transition-transform duration-300 transform hover:scale-105 hover:overflow-y-scroll">
-			    <img className="rounded-lg" src="https://i.ibb.co/j48xSfg/Labyrinth-Gaming.jpg" alt=""/>
-			  </div>
-			</div>
+		<div className="flex justify-between effect-logo rounded-lg p-2 gap-1">
+<div class="w-52 h-28 overflow-hidden shadow-md relative group rounded-lg effect-logo">
+  <div class="h-full animation-scroll">
+    <img class="rounded-lg" src="https://i.ibb.co/j48xSfg/Labyrinth-Gaming.jpg" alt=""/>
+  </div>
+  <div class="opacity-0 transition-opacity duration-300 absolute bg-[#d3d3d3] inset-0 flex justify-center items-center group-hover:opacity-100">
+    <button class="px-4 py-2 bg-blue-500 text-white rounded">Test</button>
+    <button class="px-4 py-2 bg-blue-500 text-white rounded">Test</button>
+  </div>
+</div>
+
+
+
 			<div className="relative w-52 h-28 overflow-hidden rounded-lg">
 			  <div className="absolute top-0 left-0 w-full h-full transition-transform duration-300 transform hover:scale-105 hover:overflow-y-scroll">
 			    <img className="rounded-lg" src="https://i.ibb.co/j48xSfg/Labyrinth-Gaming.jpg" alt=""/>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,21 +21,23 @@ const Home = () => {
 		<div>
 			<div className="flex justify-between gap-8">
 					<div className="flex flex-col">
-						<div className="flex justify-between gap-20">
-							<img src="https://i.ibb.co/2gmD38q/ProPic.jpg" className="w-36 h-36 p-2 effect-logo rounded-lg" alt="Profile Photo"/>
+						<div className="flex justify-between gap-20 mb-3">
+							{/*<img src="https://i.ibb.co/2gmD38q/ProPic.jpg" className="w-32 h-32 p-2 effect-logo rounded-lg" alt="Profile Photo"/>*/}
 							<div className="p-2 effect-logo rounded-lg">
 
 								<GitHubCalendar hideTotalCount hideColorLegend username="gwynbleiddrivia" colorScheme='light' blockRadius='3' labels="My Github calendar" transformData={selectLastHalfYear}/>
 								<p className="font-sm effect-logo w-fit mx-auto m-1 p-1 rounded-lg">Github Contributions</p>
 							</div>
+							<img className="flicker-animation w-2/5 mx-auto effect-logo rounded-lg" src="https://i.ibb.co/hFrSbQ6/wordcloud-1.png" alt=""/>
+						</div>
+						<div className="flex flex-col">
+						<DisplayProjects></DisplayProjects>
+						<p>My 3 best projects</p>
 						</div>
 					
-						<div>aasa</div>  
 					</div>
 				<div>
 
-					<DisplayProjects></DisplayProjects>
-					<p className="font-sm">My Best 3 Projects</p>
 				</div>
 			</div>
 
